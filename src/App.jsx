0871ecdafe24fe81f237bb995282341da5ef9443
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-04-14 00:10:42 +0200

    Update App.css and App.jsx to add a shopping list with items and delete buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <main className="App">
+      <div className="header">
+        <h1>Shopping List</h1>
+        <h3 className="completed">You are done!</h3>
+        <input type="text" placeholder="Add an item" />
+      </div>
+      <ul className="list">
+        <li>
+          <input type="checkbox" />
+          <span>Apples</span>
+          <button className="delete">Delete</button>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <span>Bananas</span>
+          <button className="delete">Delete</button>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <span>Carrots</span>
+          <button className="delete">Delete</button>
+        </li>
+      </ul>
+    </main>
+  );
 }
 
 export default App;
